c795211ff50b27e4eda8d4962fe6cdac955a29e7
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-19 17:05:51 +0100

    and not

--- special operators.py
+++ special operators.py
@@ -1,5 +1,5 @@
-is_Nice = True
-is_Fine = True
+is_Nice = False
+is_Fine = False
 
 if is_Fine and is_Nice: 
     print('love in the air')
@@ -10,3 +10,6 @@ if is_Nice or is_Fine:
     print('nice and flashy')
 else:
     print('not nice')
+
+if is_Fine and not is_Fine:
+    print('nicely done')
