10d41e4ca3e0d5db2bbd05d147048c7910871560
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-19 17:27:46 +0100

    optional operators

---
+++ more.py
@@ -0,0 +1,17 @@
+
+
+Text = 20
+
+if Text == 22:
+    print('hot text')
+else:
+    print('collect them all')
+
+name = input('enter your name : ' )
+
+if len(name) > 5:
+    print('correct guy')
+else: 
+    print('not enough')
+
+corn = input('input your phone number : ' )
