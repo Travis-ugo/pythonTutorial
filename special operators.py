ed8686449f8e0145ebf6ecc4c80dce8e74ce2f64
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-19 16:55:52 +0100

    operators

---
+++ special operators.py
@@ -0,0 +1,12 @@
+is_Nice = True
+is_Fine = True
+
+if is_Fine and is_Nice: 
+    print('love in the air')
+else:
+    print('go to sleep')
+
+if is_Nice or is_Fine:
+    print('nice and flashy')
+else:
+    print('not nice')
