d9081c5c8933ed2ef6aeb780bdf4ce061daf9572
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-21 17:37:17 +0100

    Age

---
+++ return.py
@@ -0,0 +1,17 @@
+
+def square(numbers):
+   return  2020 - numbers
+try:
+    age = square(int(input('date of birth : '))) 
+except ValueError:
+    goat = 'not valid'
+    print(goat)
+
+#age = square
+name = input(' name : ')
+other_name = input('other name : ')
+
+print(f'hey {name} {other_name} you are {int(age)} years old this year ')
+
+
+   
