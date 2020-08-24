91b65e4b3d1621aed5bb6ec86effc5b6dc4e3622
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2020-08-24 16:50:45 +0100

    Class

---
+++ Class.py
@@ -0,0 +1,17 @@
+# A Class in python is like an object constuctor or a blueprint for creating object.
+# all classes have function called __init__(), which always executes when the class 
+# is being initiated.
+# use the __init__() function to assign values to object properties, or other operationns
+# that are nessesary to do when th object is created
+
+class Person :
+    def __init__(self, name , age , year) :
+        self.name = name
+        self.age = age
+        self.year = year
+
+    def Fun(self) :
+        print('cally got them boys' + self.name + str(int( self.age)) )
+
+clout = Person(' nana', 27 , 1999) 
+clout.Fun()
